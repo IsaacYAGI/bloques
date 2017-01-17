4d4886ed3e8c9d99ded7ddcbe25373761480ec42
Isaac Yriarte <isaacyriarte@gmail.com>
2017-01-17 08:35:04 -0400

    Se programa el bote de la pelota y se configuran los rigidBody para que la misma rebote contra los objetos. Por otra parte se inclinaron las columnas para arreglar el bug en donde la pelota puede quedar botando infinitamente y se escribieron los metodos para resetear y detener la pelota que seran aplicados posteriormente

---
+++ Assets/Codigo/Pelota.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pelota : MonoBehaviour {
+
+	public float velocidadInicial = 600f;
+
+	public Rigidbody rig;
+
+	public bool isJuegoIniciado = false;
+
+	Vector3 posInicial;
+
+	public Transform barra;
+
+	/*
+	void Awake(){
+		rig = GetComponent<Rigidbody> ();
+	}
+
+	*/
+	/*
+	void Awake(){
+		barra = GetComponentInParent<Transform>();
+	}
+	*/
+
+
+	// Use this for initialization
+	void Start () {
+		posInicial = transform.position;
+	}
+
+	public void Reset(){
+		transform.position = posInicial;
+		transform.SetParent (barra);
+		isJuegoIniciado = false;
+		//rig.isKinematic = true;
+		//rig.velocity = Vector3.zero;
+		DetenerMovimiento();
+	}
+
+	public void DetenerMovimiento(){
+		rig.isKinematic = true;
+		rig.velocity = Vector3.zero;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!isJuegoIniciado && Input.GetButtonDown ("Fire1")) {
+			isJuegoIniciado = true;
+			transform.SetParent (null);
+			rig.isKinematic = false;
+			rig.AddForce (new Vector3(velocidadInicial,velocidadInicial,0));
+		}
+	}
+}
