84451765a84f31cd8403d09a17a00b625808250e
Isaac Yriarte <isaacyriarte@gmail.com>
2017-01-18 18:54:44 -0400

    Se destruyen los bloques al impactar la pelota con los bloques. Tambien se le agrego el efecto de particulas dinamicamente en el codigo

---
+++ Assets/Codigo/Bloque.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque : MonoBehaviour {
+
+	public GameObject efectoParticulas;
+
+	//is Trigger desactivado, los objetos son solidos
+	void OnCollisionEnter(){
+		Instantiate (efectoParticulas,transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+
+	//Is trigger activado, los objetos se atraviesan
+	/*void OnTriggerEnter(){
+		
+		
+	}*/
+}
