71f937fa8a9ba9d0e550a7d79fb9dd614583ae3b
Isaac Yriarte <isaacyriarte@gmail.com>
2017-01-19 13:28:22 -0400

    Se agrega el sistema de vidas donde se inicia con una cantidad de vidas definida para posteriormente ir decrementando cuando se toque el piso

---
+++ Assets/Codigo/Vidas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vidas : MonoBehaviour {
+
+	public static int vidas = 3;
+
+	public Text textoVidas;
+	public Pelota pelota;
+	public Barra barra;
+
+	// Use this for initialization
+	void Start () {
+		//textoVidas.text = "Vidas: " + Vidas.vidas;
+		ActualizarMarcadorVidas();
+	}
+	
+	public void PerderVida(){
+		Vidas.vidas--;
+		ActualizarMarcadorVidas ();
+		barra.Reset ();
+		pelota.Reset ();
+	}
+
+	void ActualizarMarcadorVidas(){
+		textoVidas.text = "Vidas: " + Vidas.vidas;
+	}
+}
