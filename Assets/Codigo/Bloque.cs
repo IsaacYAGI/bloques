0d18b9c7823fd185a3c4d7891eb4c0a8a81ec105
Isaac Yriarte <isaacyriarte@gmail.com>
2017-01-19 14:49:50 -0400

    Se agrega el sistema de puntos permitiendo que al destruir un bloque, se sume un punto en la pantalla

--- Assets/Codigo/Bloque.cs
+++ Assets/Codigo/Bloque.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Bloque : MonoBehaviour {
 
 	public GameObject efectoParticulas;
+	public Puntos puntos;
 
 	//is Trigger desactivado, los objetos son solidos
 	void OnCollisionEnter(){
 		Instantiate (efectoParticulas,transform.position, Quaternion.identity);
+		puntos.GanarPunto ();
 		Destroy (gameObject);
 	}
 
---
+++ Assets/Codigo/Puntos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puntos : MonoBehaviour {
+	public static int puntos = 0;
+	public Text textoPuntos;
+
+	void ActualizarMarcadorPuntos(){
+		textoPuntos.text = "Puntos: " + Puntos.puntos;
+	}
+
+	void Start(){
+		ActualizarMarcadorPuntos ();
+	}
+
+	public void GanarPunto(){
+		Puntos.puntos++;
+		ActualizarMarcadorPuntos ();
+	}
+
+}
