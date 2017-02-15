69249da0277b28866d0b239111ec4f29f6a9bfc4
Isaac Yriarte <isaacyriarte@gmail.com>
2017-02-15 19:18:07 -0400

    Se agregan comentarios de las modificaciones para reproducir el sonido de error al chocar contra el suelo

--- Assets/Codigo/SonidosPelota.cs
+++ Assets/Codigo/SonidosPelota.cs
@@ -8,6 +8,7 @@ public class SonidosPelota : MonoBehaviour {
 
 	public AudioSource rebote;
 	public AudioSource punto;
+	//Se agrega el Audiosource para reproducir el sonido de error
 	public AudioSource error;
 
 	void OnCollisionEnter(Collision otro){
@@ -18,11 +19,13 @@ public class SonidosPelota : MonoBehaviour {
 		}
 	}
 
+	//Se agrega el metodo OnTriggerEnter debido a que el suelo posee el isTrigger activado
 	void OnTriggerEnter(Collider otro){
-		if (Vidas.vidas != 0) {
+		if (Vidas.vidas != 0) {//Si las vidas son diferentes de 0, es decir, se puede continuar jugando, se reproduce el sonid de error
 			if (otro.gameObject.CompareTag ("Suelo")) {
 				error.Play ();
 			}
 		}
+		//En caso de que no se pueda continuar jugando el gestor del juego hara sonar el sonido de Game Over
 	}
 }
