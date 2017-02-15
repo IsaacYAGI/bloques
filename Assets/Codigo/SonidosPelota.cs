143c9e640c37c2b4b4c6855fe3a9af6d547468cb
Isaac Yriarte <isaacyriarte@gmail.com>
2017-02-15 18:22:59 -0400

    Se agregan los sonidos de rebote y al destruir un bloque

---
+++ Assets/Codigo/SonidosPelota.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidosPelota : MonoBehaviour {
+
+	//Referencias a los audiosource
+
+	public AudioSource rebote;
+	public AudioSource punto;
+
+	void OnCollisionEnter(Collision otro){
+		if (otro.gameObject.CompareTag ("Bloque")) {
+			punto.Play ();
+		} else {
+			rebote.Play ();
+		}
+	}
+}
