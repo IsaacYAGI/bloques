f4e7b509d0327818db316fe43484e8e90f9cd95c
Isaac Yriarte <isaacyriarte@gmail.com>
2017-04-14 11:14:01 -0400

    Se agrega de forma comentada la posibilidad de cambiar el color de los bloques

--- Assets/Codigo/BloqueLvl2.cs
+++ Assets/Codigo/BloqueLvl2.cs
@@ -10,6 +10,14 @@ public class BloqueLvl2 : Bloque{
 	void OnCollisionEnter(){
 		if (golpesRecibidos != CANTIDAD_GOLPES_PARA_DESTRUIR) {
 			golpesRecibidos++;
+
+			//Si en el proximo golpe se va a destruir el bloque
+			/*
+			if (isProximoGolpeDestruye()) {
+				//Se cambia el color del bloque
+
+			}
+			*/
 		}else{
 			Instantiate (efectoParticulas,transform.position, Quaternion.identity);
 			Destroy (gameObject);
@@ -18,4 +26,9 @@ public class BloqueLvl2 : Bloque{
 		}
 
 	}
+	/*
+	bool isProximoGolpeDestruye(){
+		return golpesRecibidos == CANTIDAD_GOLPES_PARA_DESTRUIR;
+	}
+	*/
 }
