46cd1d910920e464b2af097b6479168bf85640bf
Isaac Yriarte <isaacyriarte@gmail.com>
2017-04-14 10:50:55 -0400

    Se agregan los bloques level 2 que heredan de los bloques comunes, pero poseen un atributo de cantidad de golpes para configurar cuantos son necesarios para destruir el bloque. Tambien se modifico el nivel Nivel02 para agregar bloques de este tipo

---
+++ Assets/Codigo/BloqueLvl2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloqueLvl2 : Bloque{
+
+	public int CANTIDAD_GOLPES_PARA_DESTRUIR = 2;
+
+	void OnCollisionEnter(){
+		if (CANTIDAD_GOLPES_PARA_DESTRUIR == 2) {
+			CANTIDAD_GOLPES_PARA_DESTRUIR--;
+		}else{
+			Instantiate (efectoParticulas,transform.position, Quaternion.identity);
+			Destroy (gameObject);
+			transform.SetParent (null);
+			puntos.GanarPunto ();
+		}
+
+	}
+}
